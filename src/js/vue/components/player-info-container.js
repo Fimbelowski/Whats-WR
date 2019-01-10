025c3c77f5415f104f5d03f08ce842797dd2e7c0
unknown <ChrFimbel@gmail.com>
2019-01-10 15:55:58 -0700

    Started on various design work. Added social media icons.

--- src/js/vue/components/player-info-container.js
+++ src/js/vue/components/player-info-container.js
@@ -1,6 +1,6 @@
-Vue.component('players-info', {
+Vue.component('player-info-container', {
     props: ['players'],
-    template: ' <section class="l-players-info">\
+    template: ' <section class="player-info-container">\
                     <player-info v-for="player in players" :player="player" :key="player.id"></player-info>\
                 </section>'
 });
--- src/js/vue/components/player-info.js
+++ src/js/vue/components/player-info.js
@@ -1,16 +1,14 @@
 Vue.component('player-info', {
     props: ['player'],
-    template: ' <div>\
-                    <a v-if="player.src" :href="player.src"><h3>{{ player.name }}</h3></a>\
+    template: ' <div class="player-info">\
+                    <a v-if="player.src" :href="player.src" class="player-src-link"><h3>{{ player.name }}</h3></a>\
                     <h3 v-else>{{ player.name }}</h3>\
-                    \
-                    <a v-if="player.twitch" :href="player.twitch"><p>Twitch</p></a>\
-                    <p v-else>No Twitch</p>\
-                    \
-                    <a v-if="player.youtube" :href="player.youtube"><p>YouTube</p></a>\
-                    <p v-else>No YouTube</p>\
-                    \
-                    <a v-if="player.twitter" :href="player.twitter"><p>Twitter</p></a>\
-                    <p v-else>No Twitter</p>\
+                    <div class="player-social-container">\
+                        <a v-if="player.twitch" :href="player.twitch"><img src="/dist/images/Glitch_Purple_RGB.png" class="player-social-icon"></a>\
+                        \
+                        <a v-if="player.youtube" :href="player.youtube"><img src="/dist/images/yt_icon_rgb.png" class="player-social-icon"></a>\
+                        \
+                        <a v-if="player.twitter" :href="player.twitter"><img src="/dist/images/Twitter_Logo_Blue.png" class="player-social-icon"></a>\
+                    </div>\
                 </div>'
 });
