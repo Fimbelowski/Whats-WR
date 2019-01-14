e5d7e7076bd8fa0e83c4100e816479a2bb2c6852
unknown <ChrFimbel@gmail.com>
2019-01-13 23:30:02 -0700

    Updated runner social media link icons.

--- src/js/vue/components/player-info.js
+++ src/js/vue/components/player-info.js
@@ -6,9 +6,9 @@ Vue.component('player-info', {
                     <div class="player-social-container">\
                         <a v-if="player.twitch" :href="player.twitch"><img src="/dist/images/Glitch_Purple_RGB.png" class="player-social-icon"></a>\
                         \
-                        <a v-if="player.youtube" :href="player.youtube"><img src="/dist/images/yt_icon_rgb.png" class="player-social-icon"></a>\
+                        <a v-if="player.youtube" :href="player.youtube"><img src="/dist/images/youtube.png" class="player-social-icon"></a>\
                         \
-                        <a v-if="player.twitter" :href="player.twitter"><img src="/dist/images/Twitter_Logo_Blue.png" class="player-social-icon"></a>\
+                        <a v-if="player.twitter" :href="player.twitter"><img src="/dist/images/twitter.png" class="player-social-icon"></a>\
                     </div>\
                 </div>'
 });
