f104b42844def70aefcd35a49bb266076439316f
unknown <ChrFimbel@gmail.com>
2019-01-14 20:07:31 -0700

    Changed the player info header to be reactive based on the number of players.

--- src/js/vue/components/player-info-container.js
+++ src/js/vue/components/player-info-container.js
@@ -1,8 +1,13 @@
 Vue.component('player-info-container', {
     props: ['players'],
+    computed: {
+        playersHeader: function() {
+            return (this.players.length === 1) ? 'Runner' : 'Runners';
+        }
+    },
     template: ' <section class="player-info-container">\
                     <div class="player-info-container-header">\
-                        <h3>Runners</h3>\
+                        <h3>{{ playersHeader }}</h3>\
                     </div>\
                     <player-info v-for="player in players" :player="player" :key="player.id"></player-info>\
                 </section>'
