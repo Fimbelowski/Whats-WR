194bae6fadef357c4d9c61198f282f8a54e90c63
unknown <ChrFimbel@gmail.com>
2019-01-14 20:39:41 -0700

    Updated social media icons to be all white instead of brand colors.

--- src/js/vue/components/player-info.js
+++ src/js/vue/components/player-info.js
@@ -4,11 +4,11 @@ Vue.component('player-info', {
                     <a v-if="player.src" :href="player.src"><h3>{{ player.name }}</h3></a>\
                     <h3 v-else>{{ player.name }}</h3>\
                     <div class="player-social-container">\
-                        <a v-if="player.twitch" :href="player.twitch"><img src="/dist/images/Glitch_Purple_RGB.png" class="player-social-icon"></a>\
+                        <a v-if="player.twitch" :href="player.twitch"><img src="/dist/images/Glitch_White_RGB.png" class="player-social-icon"></a>\
                         \
-                        <a v-if="player.youtube" :href="player.youtube"><img src="/dist/images/youtube.png" class="player-social-icon"></a>\
+                        <a v-if="player.youtube" :href="player.youtube"><img src="/dist/images/yt_icon_mono_dark.png" class="player-social-icon"></a>\
                         \
-                        <a v-if="player.twitter" :href="player.twitter"><img src="/dist/images/twitter.png" class="player-social-icon"></a>\
+                        <a v-if="player.twitter" :href="player.twitter"><img src="/dist/images/Twitter_Logo_White.png" class="player-social-icon"></a>\
                     </div>\
                 </div>'
 });
