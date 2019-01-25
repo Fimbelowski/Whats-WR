3aa40dbc94939b87def635ec943d5103bfa27c65
Chris (Work) <ChrFimbel@gmail.com>
2019-01-24 17:21:39 -0700

    Changed player social media container to only render when a player has social media.

--- src/js/vue/components/player-info.js
+++ src/js/vue/components/player-info.js
@@ -1,9 +1,14 @@
 Vue.component('player-info', {
     props: ['player'],
+    computed: {
+        hasSocial: function() {
+            return this.player.twitch || this.player.youtube || this.player.twitter;
+        }
+    },
     template: ' <div class="player-info">\
                     <a v-if="player.src" :href="player.src"><h3>{{ player.name }}</h3></a>\
                     <h3 v-else>{{ player.name }}</h3>\
-                    <div class="player-social-container">\
+                    <div v-if="hasSocial" class="player-social-container">\
                         <a v-if="player.twitch" :href="player.twitch"><img src="/dist/images/Glitch_White_RGB.png" class="player-social-icon"></a>\
                         \
                         <a v-if="player.youtube" :href="player.youtube"><img src="/dist/images/yt_icon_mono_dark.png" class="player-social-icon"></a>\
