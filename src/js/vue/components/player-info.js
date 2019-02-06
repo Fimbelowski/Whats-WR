3a8141f17fe0550c4cbf00da4d477f695af9a8bb
unknown <ChrFimbel@gmail.com>
2019-02-05 19:51:22 -0700

    Fixed an issue where runner names would not render.

--- src/js/vue/components/player-info.js
+++ src/js/vue/components/player-info.js
@@ -2,7 +2,7 @@ Vue.component('player-info', {
     props: ['player'],
     computed: {
         name: function() {
-            if(this.player.rel) {
+            if(this.player.rel === 'guest') {
                 return this.player.name;
             } else {
                 if(this.player.names.japanese && this.player.names.international) {
@@ -23,9 +23,7 @@ Vue.component('player-info', {
                     <h3 v-else class="player-text">{{ name }}</h3>\
                     <div v-if="hasSocial" class="player-social-container">\
                         <a v-if="player.twitch" :href="player.twitch.uri"><img src="/dist/images/Glitch_White_RGB.png" class="player-social-icon"></a>\
-                        \
                         <a v-if="player.youtube" :href="player.youtube.uri"><img src="/dist/images/yt_icon_mono_dark.png" class="player-social-icon"></a>\
-                        \
                         <a v-if="player.twitter" :href="player.twitter.uri"><img src="/dist/images/Twitter_Logo_White.png" class="player-social-icon"></a>\
                     </div>\
                 </div>'
