6810d40011068fcce786a2da49b63b47666c1dda
Christopher <ChrFimbel@gmail.com>
2019-05-19 23:33:46 -0600

    Added getRandomCollectionOfCategories().

--- whats-wr/src/App.vue
+++ whats-wr/src/App.vue
@@ -4,6 +4,8 @@
 
 <script>
 import axios from 'axios';
+import collect from 'collect.js';
+import rn from 'random-number';
 
 export default {
   name: 'App',
@@ -17,8 +19,30 @@ export default {
     this.getTotalNumberOfGames();
   },
   methods: {
+    /** @return {void} */
+    getRandomCollectionOfCategories() {
+      const randomOffset = rn({
+        min: 0,
+        max: this.totalNumberOfGames - 21,
+        integer: true,
+      });
+
+      let categories = collect([]);
+
+      axios.get(`/games?embed=categories&offset=${randomOffset}`)
+        .then((items) => {
+          items.data.data.forEach((item) => {
+            categories = categories.concat(item.categories);
+          });
+
+          categories = categories.filter(item => item.type === 'per-game');
+          console.log(categories);
+        });
+    },
+
+    /** @return {void} */
     getTotalNumberOfGames() {
-      axios.get(`/games?_bulk=yes&max=1000&offset=${this.totalNumberOfGamesStartingOffset}`)
+      axios.get(`/games?_bulk=yes&max=1000&offset=${this.totalNumberOfGamesOffset}`)
         .then((items) => {
           if (items.data.pagination.size < 1000) {
             this.totalNumberOfGames = this.totalNumberOfGamesOffset + items.data.pagination.size;
@@ -27,6 +51,8 @@ export default {
 
           this.totalNumberOfGamesStartingOffset += 1000;
           this.getTotalNumberOfGames();
+        }).finally(() => {
+          this.getRandomCollectionOfCategories();
         });
     },
   },
