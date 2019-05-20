d4650d0c2053761210424a471776b4425a9604af
Christopher <ChrFimbel@gmail.com>
2019-05-19 22:26:26 -0600

    Configured axios base URL and added fetching the total number of games on SRcom.

--- whats-wr/src/App.vue
+++ whats-wr/src/App.vue
@@ -3,7 +3,32 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'App',
+  data() {
+    return {
+      totalNumberOfGames: null,
+      totalNumberOfGamesOffset: 15000,
+    };
+  },
+  created() {
+    this.getTotalNumberOfGames();
+  },
+  methods: {
+    getTotalNumberOfGames() {
+      axios.get(`/games?_bulk=yes&max=1000&offset=${this.totalNumberOfGamesStartingOffset}`)
+        .then((items) => {
+          if (items.data.pagination.size < 1000) {
+            this.totalNumberOfGames = this.totalNumberOfGamesOffset + items.data.pagination.size;
+            return;
+          }
+
+          this.totalNumberOfGamesStartingOffset += 1000;
+          this.getTotalNumberOfGames();
+        });
+    },
+  },
 };
 </script>
--- whats-wr/src/main.js
+++ whats-wr/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import axios from 'axios';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+
+axios.defaults.baseURL = 'https://www.speedrun.com/api/v1';
 
 new Vue({
   render: h => h(App),
