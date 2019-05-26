13f471788287672c05d59800c815f1ea8bb4d087
Christopher <ChrFimbel@gmail.com>
2019-05-26 00:28:21 -0600

    Setup Axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import Axios from 'axios';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+
+Axios.defaults.baseUrl = 'https://www.speedrun.com/api/v1';
 
 new Vue({
   render: h => h(App),
