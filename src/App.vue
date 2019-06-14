3a7e73bac69f53003fbd46b689ed23ba77901717
Christopher <ChrFimbel@gmail.com>
2019-06-13 23:47:18 -0600

    Updated axios defaults. Furthered category filtering.

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,22 @@
 <script>
 import axios from 'axios';
 import rn from 'random-number';
+import parse from 'url-parse';
 
 export default {
   name: 'App',
   data() {
     return {
-      totalNumberOfGames: 15000,
+      speedrunDotComApi: axios.create({
+        baseURL: 'https://www.speedrun.com/api/v1',
+        transformResponse: axios.defaults.transformResponse.concat(data => data.data),
+      }),
+      totalNumberOfGames: 16000,
+      validHostNames: [
+        'www.youtube.com',
+        'youtu.be',
+        'www.twitch.tv',
+      ],
     };
   },
   created() {
@@ -32,7 +42,6 @@ export default {
 
     /** @return {void} */
     async findRun() {
-      let groupOfCategories;
       let groupOfGames;
 
       try {
@@ -41,13 +50,54 @@ export default {
         //
       }
 
-      groupOfGames = groupOfGames.data.data;
-      groupOfCategories = this.extractCategoriesFrom(groupOfGames);
+      groupOfGames = groupOfGames.data;
+      const groupOfCategories = this.extractCategoriesFrom(groupOfGames);
+
+      const promises = [];
+
+      for (let i = 0; i < 3; i += 1) {
+        const categoryId = groupOfCategories[rn({
+          integer: true,
+          min: 0,
+          max: groupOfCategories.length - 1,
+        })].id;
+
+        promises.push(this.speedrunDotComApi.get(`/categories/${categoryId}/records`, {
+          params: {
+            top: 1,
+          },
+        }));
+      }
+
+      let categoryRecords;
+
+      Promise.all(promises)
+        .then((values) => {
+          categoryRecords = values.filter((category) => {
+            return (
+              category.data[0].runs.length
+              && category.data[0].runs[0].run.videos
+              && category.data[0].runs[0].run.videos.links
+              && category.data[0].runs[0].run.videos.links.length
+            );
+          });
+
+          categoryRecords = categoryRecords.filter((category) => {
+            const url = parse(category.data[0].runs[0].run.videos.links[0].uri);
+            console.log(url);
+
+            return this.validHostNames.includes(url.hostname);
+          });
+
+          categoryRecords.forEach((category) => {
+            console.log(category.data[0].runs[0].run.videos.links[0].uri);
+          });
+        });
     },
 
     /** @return {void} */
     findTotalNumberOfGames() {
-      axios.get('/games', {
+      this.speedrunDotComApi.get('/games', {
         params: {
           _bulk: 'yes',
           max: 1000,
@@ -55,7 +105,7 @@ export default {
         },
       })
         .then((response) => {
-          const count = response.data.data.length;
+          const count = response.data.length;
           this.totalNumberOfGames += count;
 
           if (count === 1000) {
@@ -69,7 +119,7 @@ export default {
 
     /** @return {array} */
     getRandomGroupOfGames() {
-      return axios.get('/games', {
+      return this.speedrunDotComApi.get('/games', {
         params: {
           embed: 'categories',
           offset: rn({
--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import Vue from 'vue';
-import axios from 'axios';
 import App from './App.vue';
-
-axios.defaults.baseURL = 'https://www.speedrun.com/api/v1';
 
 Vue.config.productionTip = false;
 
