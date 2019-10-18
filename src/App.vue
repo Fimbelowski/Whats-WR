883bd3ed6bb672f2cbc32f2920b84312d1a1da8d
Fimbelowski <chrfimbel@gmail.com>
2019-10-18 02:44:05 -0600

    Clean directory.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
--- src/scripts/components/App.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div id='app'></div>
-</template>
