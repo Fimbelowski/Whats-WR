1bed2bf7b5c4a9a2f073b086ad4eabbadf38a188
Christopher <Fimbelowski@users.noreply.github.com>
2021-06-24 06:36:54 -0600

    Setup eslint (#21)
    * WIP
    
    * Updated linter pattern, fixed errors.
    
    * Update lint command to search more broadly. Updated rules. Fixed errors.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,28 +2,52 @@
   <h1>{{ msg }}</h1>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+    <a
+      href="https://vitejs.dev/guide/features.html"
+      target="_blank"
+    >
       Vite Documentation
     </a>
     |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    <a
+      href="https://v3.vuejs.org/"
+      target="_blank"
+    >
+      Vue 3 Documentation
+    </a>
   </p>
 
-  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
+  <button
+    type="button"
+    @click="state.count++"
+  >
+    count is: {{ state.count }}
+  </button>
   <p>
     Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    <code>
+      components/HelloWorld.vue
+    </code> to test hot module replacement.
   </p>
 </template>
 
-<script setup>
-import { defineProps, reactive } from 'vue'
+<script>
+export default {
+  name: 'HelloWord',
 
-defineProps({
-  msg: String
-})
+  props: {
+    msg: {
+      required: true,
+      type: String,
+    },
+  },
 
-const state = reactive({ count: 0 })
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
 </script>
 
 <style scoped>
