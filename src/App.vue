04eae7177546a77bd82c14d1b583d43c68bdaf1d
Christopher <ChrFimbel@gmail.com>
2021-06-24 06:29:10 -0600

    Updated linter pattern, fixed errors.

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,16 @@
   />
 </template>
 
-<script setup>
+<script>
 import HelloWorld from './components/HelloWorld.vue';
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
