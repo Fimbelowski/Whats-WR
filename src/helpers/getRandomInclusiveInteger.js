88f8f09ae1b476f76695b8054b9631b46d5f203f
Christopher <ChrFimbel@gmail.com>
2021-09-27 17:21:56 -0600

    Make getRandomInclusiveInteger actually inclusive.

--- src/helpers/getRandomInclusiveInteger.js
+++ src/helpers/getRandomInclusiveInteger.js
@@ -1,6 +1,6 @@
 /** @return {number} */
 function getRandomInclusiveInteger(min, max) {
-  return Math.floor(Math.random() * max) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export default getRandomInclusiveInteger;
